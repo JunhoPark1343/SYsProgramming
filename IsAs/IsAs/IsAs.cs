7d7337f8a0b2d4b203ff788f83825fd3be68f0d6
JunhoPark1343 <80228029+JunhoPark1343@users.noreply.github.com>
2021-03-15 10:32:38 +0900

    Add files via upload

---
+++ IsAs/IsAs/IsAs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class IsAs
+{
+    static void Isas(object obj)
+    {
+        Console.WriteLine(obj + " is int : " + (obj is int));
+        Console.WriteLine(obj + " is string : " + (obj is string));
+    }
+    static void AsOperator(object obj)
+    {
+        Console.WriteLine(obj + " as string == null : " + (obj as string == null));
+    }
+    public static void Main()
+    {
+        Isas(10);
+        Isas("ABC");
+        AsOperator(10);
+        AsOperator("ABC");
+    }
+}
+
